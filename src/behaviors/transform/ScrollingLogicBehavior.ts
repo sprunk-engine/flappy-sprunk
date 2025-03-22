a5e5053e5dc4e30ba3cb0c6029cbefee16bfafec
eliott <eliott.jaquier@gmail.com>
2025-03-22 21:48:58 +0100

    feat: expose scolling speed

--- src/behaviors/transform/ScrollingLogicBehavior.ts
+++ src/behaviors/transform/ScrollingLogicBehavior.ts
@@ -4,7 +4,8 @@ import { LogicBehavior } from "sprunk-engine";
  * Scrolling logic behavior for creating horizontal movement effects
  */
 export class ScrollingLogicBehavior extends LogicBehavior<void> {
-    private _scrollSpeed: number;
+    public scrollSpeed: number;
+
     private readonly _resetPosition: number;
     private readonly _startPosition: number;
     
@@ -12,14 +13,14 @@ export class ScrollingLogicBehavior extends LogicBehavior<void> {
         super();
         this._startPosition = startPosition;
         this._resetPosition = resetPosition;
-        this._scrollSpeed = scrollSpeed;
+        this.scrollSpeed = scrollSpeed;
     }
     
     public tick(deltaTime: number): void {
         // Update texture offset for scrolling effect
         // Note: In a real implementation, we would modify texture coordinates
         // As a simplified version, we're moving the object itself
-        this.gameObject.transform.position.x -= this._scrollSpeed * deltaTime;
+        this.gameObject.transform.position.x -= this.scrollSpeed * deltaTime;
         
         // Reset position when it has scrolled too far
         if (this.gameObject.transform.position.x < this._resetPosition) {

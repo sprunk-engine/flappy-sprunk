0dee7b86e62a262e2c23498e6b60255d2a6b55aa
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-26 22:13:56 +0100

    refactor: update the bird logic in order to use the new physics props

--- src/behaviors/flappybird/BirdLogicBehavior.ts
+++ src/behaviors/flappybird/BirdLogicBehavior.ts
@@ -1,5 +1,6 @@
-import {Inject, LogicBehavior, Vector3} from "sprunk-engine";
-import {FlappGameManagerLogicBehavior} from "./FlappGameManagerLogicBehavior.ts";
+import { LogicBehavior, Vector2, Inject } from "sprunk-engine";
+import { FlappGameManagerLogicBehavior } from "./FlappGameManagerLogicBehavior";
+import { BirdGameObject } from "../../gameobjects/BirdGameObject";
 
 /**
  * Bird physics and behavior logic
@@ -8,51 +9,45 @@ export class BirdLogicBehavior extends LogicBehavior<void> {
     @Inject(FlappGameManagerLogicBehavior, true)
     private _gameManager!: FlappGameManagerLogicBehavior;
 
-    private _velocity: Vector3 = new Vector3(0, 0, 0);
-    private _gravity: number = 9.81*2;
-    private _flapStrength: number = 9.81/1.5;
-    private _rotation: number = 0;
+    private _flapForce: number = 12;
+    private _physicsEnabled: boolean = false;
 
     protected onEnable() {
         super.onEnable();
         this._gameManager.birdTransform = this.gameObject.transform;
     }
 
-    public tick(deltaTime: number): void {
-        if(!this._gameManager.isGamePlaying()) return;
-        
-        // Apply gravity
-        this._velocity.y -= this._gravity * deltaTime;
-        
-        // Apply velocity to position
-        this.gameObject.transform.position.add(
-            new Vector3(0, this._velocity.y * deltaTime, 0)
-        );
-        
-        // Rotate bird based on velocity (diving down or flapping up)
-        this._rotation = Math.max(-Math.PI / 4, Math.min(Math.PI / 4, this._velocity.y * 0.5));
-        this.gameObject.transform.rotation.setFromEulerAngles(0, 0, this._rotation);
-        
-        // Check if bird has hit the ground
-        if (this.gameObject.transform.position.y < -4) {
-            this.die();
-        }
-    }
-    
     /**
-     * Make the bird flap upwards
+     * Make the bird flap upwards using physics force
      */
     public flap(): void {
+        console.log("Flapping");
+        // First notify game manager of flap attempt
         this._gameManager.wantToFlap();
-        if(!this._gameManager.isGamePlaying()) return;
-        this._velocity.y = this._flapStrength;
+        
+        // If game isn't playing, don't apply forces
+        if (!this._gameManager.isGamePlaying()) return;
+        
+        // Enable physics if this is the first flap
+        if (!this._physicsEnabled) {
+            this._physicsEnabled = true;
+            (this.gameObject as BirdGameObject).enablePhysics();
+        }
+        
+        // Get rigidbody and apply force
+        const bird = this.gameObject as BirdGameObject;
+        const rigidbody = bird.getRigidbody();
+
+        
+        if (rigidbody) {
+            rigidbody.addForce(new Vector2(0, 100));
+        }
     }
-    
+
     /**
      * Kill the bird and stop movement
      */
     public die(): void {
         this._gameManager.gameOver();
-        this._velocity.set(0, 0, 0);
     }
 } 
